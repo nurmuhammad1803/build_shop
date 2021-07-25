0491e2cfc50b8130cdf5cf7831c78ab30feb3dc4
Nurmuhammad Abdullaev <nurmuhammadabdullaev1803@gmail.com>
2021-07-25 08:40:03 +0500

    Qurulish firmasi uchun tayyorlangan landing page tugadi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.getElementById("mySidenav").classList.toggle('show');
+  document.getElementById("bgChange").classList.toggle('show');
+  document.getElementById("changeZ").classList.toggle('show');
+}
+// change bg on scroll
+// window.addEventListener('scroll', () => {
+//   console.log(window.scrollY)
+//   if (window.screenY > 400) {
+//     document.querySelector('.header__nav').classList.toggle('scroll');
+//   }
+// })
+
+// var btnCheck = document.querySelector('.input__check')
+
+// function Checked(){
+//   btnCheck.style.background = 'url(./../images/vectors/check.png)'
+// }
